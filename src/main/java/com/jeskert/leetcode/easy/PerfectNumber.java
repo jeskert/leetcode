c0fe0db0e8194e7a566b06666e785b8ad9176458
Weiye Li <weiye.b.li@accenture.com>
2021-11-26 11:29:26 +0800

    [wyli] #N/A: 2021-11-26

---
+++ src/main/java/com/jeskert/leetcode/easy/PerfectNumber.java
@@ -0,0 +1,13 @@
+package com.jeskert.leetcode.easy;
+
+public class PerfectNumber {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        for (int i = 1; i < num / 2; i++) {
+            if (num % i == 0) {
+                sum += 2 * i;
+            }
+        }
+        return num == sum;
+    }
+}
